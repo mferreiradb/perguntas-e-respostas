17f58edbc8706a217a2eb65d702e99b1fb7dc0da
Mauricio <mauhmg@gmail.com>
2022-11-09 23:43:22 -0300

    Expressoes no html com ejs

--- server/index.js
+++ server/index.js
@@ -7,13 +7,21 @@ app.set('view engine', 'ejs');
 app.get('/:nome?/:lang?', (req, res) => {
 	let nome = req.params.nome;
 	let lang = req.params.lang;
-	let exibmsg = true;
+	let isAdm = false;
+
+	let produtos = [
+		{nome: 'Arroz', valor:12, ativo: true},
+		{nome: 'Feijao', valor: 10, ativo: false},
+		{nome: 'Batata', valor: 11.50, ativo: true},
+		{nome: 'Pao', valor: 0.50, ativo: false}
+	];
 	res.render('index', {
 		nome: nome,
 		lang: lang,
 		empresa: 'TicBox Sistemas',
 		colaboradores: 12,
-		msg: exibmsg
+		adm: isAdm,
+		produtos: produtos
 	});
 });
 
