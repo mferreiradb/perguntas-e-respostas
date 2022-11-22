9d95476e7715ba8f4f921354c5a5165461798eeb
Mauricio <mauhmg@gmail.com>
2022-11-22 20:16:49 -0300

    Alteração na estrutura de pastas

---
+++ app/models/Pergunta.js
@@ -0,0 +1,17 @@
+const db = require('./db.js');
+const Sequelize = require('sequelize');
+
+const Pergunta = db.sequelize.define('perguntas', {
+	titulo: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	descricao: {
+		type: Sequelize.TEXT,
+		allowNull: false
+	}
+});
+
+//Pergunta.sync({force: false}).then(() =>{ console.log('Tabela criada');});
+
+module.exports = Pergunta;
